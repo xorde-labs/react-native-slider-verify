9faceb5135c3645ffab7f09e415e0348084e6c0e
XT <xt@xorde.co>
2022-09-28 16:47:24 -0400

    Cleanup

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
-	// plugins: ['@babel/plugin-proposal-private-methods'],
 };
